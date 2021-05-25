8dec950d982e8b86a42fe6f2a784579c197cd417
Eduard Goriaev <eduard.goryaev@yandex.ru>
2021-05-25 15:00:38 +0300

    GitCourse modified

--- src/package1/GitCourse.java
+++ src/package1/GitCourse.java
@@ -3,5 +3,6 @@ package package1;
 public class GitCourse {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("I'm almost done with learning Git!");
     }
 }
